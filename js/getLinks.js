f251be1c852c2a1761f3c397994cb007049c66da
hambergjesse <hambergjesse@gmail.com>
2022-04-04 13:29:47 +0300

    organizing.

---
+++ js/getLinks.js
@@ -0,0 +1,10 @@
+const getLinks =
+  //main css
+  '<link rel="stylesheet" href="./style.css" />' +
+  //media queries
+  '<link rel="stylesheet" href="./css/responsive.css" />' +
+  //dark mode
+  '<link rel="stylesheet" href="./css/darkmode.css" />' +
+  '<link rel="icon" href="./images/icons/logo-black.png" />';
+
+document.getElementById("getLinks").innerHTML = getLinks;
