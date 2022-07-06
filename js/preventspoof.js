7529acf9855f5a115e7119a6b652cc41ae2c1976
Jesse <hambergjesse@gmail.com>
2022-07-06 15:45:56 +0300

    misc. adjustments

--- js/preventspoof.js
+++ js/preventspoof.js
@@ -1,6 +1,6 @@
 //Preventing URL spoofing and polluting
-var url = location.href;
-var p = url.indexOf("?");
+const url = location.href;
+const p = url.indexOf("?");
 if (p >= 1) {
   url = url.slice(0, p);
   self.location.replace(url);
